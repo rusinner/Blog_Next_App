938d0f5ca0dba0949b8527f08eb78ca419a9507d
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-17 00:25:16 +0200

    graphql request

--- blog-next-app/src/pages/index.js
+++ blog-next-app/src/pages/index.js
@@ -1,12 +1,9 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { Categories, PostCard, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React Testing", excerpt: "Learn React Testing" },
-];
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,10 +15,10 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard key={index} post={post} />
           ))}
         </div>
-        <div className="lg:col-span-4 cl-span-1">
+        <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
@@ -30,4 +27,14 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+//fetch data from graphql
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
