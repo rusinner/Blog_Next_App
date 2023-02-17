0c00abfe9d816119a8f160a27aac66d035c3d63c
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-17 18:21:59 +0200

    postwidget

--- blog-next-app/src/pages/index.js
+++ blog-next-app/src/pages/index.js
@@ -3,7 +3,6 @@ import { Categories, PostCard, PostWidget } from "../components";
 import { getPosts } from "../services";
 
 export default function Home({ posts }) {
-  console.log(posts);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
--- blog-next-app/src/services/index.js
+++ blog-next-app/src/services/index.js
@@ -1,14 +1,13 @@
 import { gql, GraphQLClient } from "graphql-request";
 
+const endpoint = process.env.NEXT_PUBLIC_GRAPHICS_ENDPOINT;
+
+const graphQLClient = new GraphQLClient(endpoint, {
+  headers: {
+    authorization: process.env.NEXT_PUBLIC_MY_TOKEN,
+  },
+});
 export const getPosts = async () => {
-  const endpoint = process.env.NEXT_PUBLIC_GRAPHICS_ENDPOINT;
-
-  const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-      authorization: process.env.NEXT_PUBLIC_MY_TOKEN,
-    },
-  });
-
   const query = gql`
     query Assets {
       postsConnection {
@@ -42,4 +41,50 @@ export const getPosts = async () => {
   const result = await graphQLClient.request(query);
 
   return result.postsConnection;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+  query getPostDetails()
+  {posts(
+    orderBy:createdAt_ASC
+    last:3
+    
+    ){
+      title
+      featuredImage{
+        url
+      }
+      createdAt
+      slug
+    }
+    }
+  `;
+  const result = await graphQLClient.request(query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await graphQLClient.request(query);
+
+  return result.posts;
 };
