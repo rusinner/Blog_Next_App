b322a73748e4807986e80d04cc1ca0c9c04a046e
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-17 17:52:21 +0200

    fixed rendering

--- blog-next-app/src/pages/index.js
+++ blog-next-app/src/pages/index.js
@@ -3,6 +3,7 @@ import { Categories, PostCard, PostWidget } from "../components";
 import { getPosts } from "../services";
 
 export default function Home({ posts }) {
+  console.log(posts);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -14,7 +15,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => {
-            return <PostCard key={index} post={post} />;
+            return <PostCard key={index} post={post.node} />;
           })}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -34,6 +35,6 @@ export async function getStaticProps() {
   const posts = (await getPosts()) || [];
 
   return {
-    props: { posts: [posts] },
+    props: { posts: posts.edges },
   };
 }
