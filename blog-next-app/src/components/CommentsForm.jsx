755c1cc21caf1b5f7a276423465ace27851640a6
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-18 14:08:15 +0200

    handle comment submission

--- blog-next-app/src/components/CommentsForm.jsx
+++ blog-next-app/src/components/CommentsForm.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const CommentsForm = () => {
-  return <div>CommentsForm</div>;
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const emailEl = useRef();
+  const nameEl = useRef();
+  const storeDataEl = useRef();
+
+  const handleCommentSubmission = () => {};
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comment Form</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <input
+          type="text"
+          ref={nameEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      {error && <p className="text-xs text-red-500">All fields are required</p>}
+      <div className="mt-8">
+        <button
+          type="button"
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer "
+          onClick={handleCommentSubmission}
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="text-xl float-right font-semibold mt-3 text-green-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CommentsForm;
