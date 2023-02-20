242b81fe737b997b4a2ed743ed0790f0e72ca554
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-20 14:24:23 +0200

    category/slug

--- blog-next-app/src/pages/category/[slug].js
+++ blog-next-app/src/pages/category/[slug].js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-import { getCategories, getCategoryPost } from "../category/slug";
+import { getCategories, getCategoryPost } from "../category";
 import { PostCard, Categories, Loader } from "../../components";
 
 const CategoryPost = ({ posts }) => {
